bc7747a63b16b2dd68abc77151ecbd66b0dda856
rubrenmar <rubrenmar@alu.edu.gva.es>
2024-10-23 10:10:24 +0200

    v0.2 - Añadidas las card views, pero por alg�n motivo, se ponen por debajo de la barra de la app

--- app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
@@ -3,18 +3,74 @@ package com.example.imcapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcCalculatorActivity : AppCompatActivity() {
+
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+    var isMaleSelected:Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_imc_calculator)
+
+        initComponents()
+        initListeners()
+        initUI()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun initComponents()
+    {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    private fun initListeners()
+    {
+        viewMale.setOnClickListener()
+        {
+            isMaleSelected = true
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener()
+        {
+            isMaleSelected = false
+            setGenderColor()
+        }
+    }
+
+    private fun initUI()
+    {
+        setGenderColor()
+    }
+
+    private fun setGenderColor()
+    {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(!isMaleSelected))
+    }
+
+    private fun getBackgroundColor(isComponentSelected:Boolean): Int {
+        val colorReference = if(isComponentSelected)
+        {
+            R.color.bg_comp_sel
+        }
+        else
+        {
+            R.color.bg_comp
+        }
+
+        return ContextCompat.getColor(this, colorReference)
     }
 }
