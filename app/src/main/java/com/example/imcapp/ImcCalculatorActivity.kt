d02cc6a5ea95e147231edf7af22dcf06a49383a2
rubrenmar <rubrenmar@alu.edu.gva.es>
2024-10-23 12:32:05 +0200

    v0.3 - Slider funciona, seguir con botones de peso y edad

--- app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
@@ -1,39 +1,45 @@
 package com.example.imcapp
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.RangeSlider
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
     private lateinit var viewMale:CardView
     private lateinit var viewFemale:CardView
     var isMaleSelected:Boolean = true
+    private lateinit var tvHeight:TextView
+    private lateinit var rsHeight:RangeSlider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_imc_calculator)
-
-        initComponents()
-        initListeners()
-        initUI()
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initComponents()
+        initListeners()
+        initUI()
     }
 
     private fun initComponents()
     {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
     }
 
     private fun initListeners()
@@ -43,10 +49,15 @@ class ImcCalculatorActivity : AppCompatActivity() {
             isMaleSelected = true
             setGenderColor()
         }
+
         viewFemale.setOnClickListener()
         {
             isMaleSelected = false
             setGenderColor()
+        }
+
+        rsHeight.addOnChangeListener{ _, value, _ ->
+            tvHeight.text = DecimalFormat("#.##").format(value) + " cm"
         }
     }
 
