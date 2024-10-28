696c40b507b1bc4dcb54cabe8ed5370adc2f7727
rubrenmar <rubrenmar@alu.edu.gva.es>
2024-10-28 14:05:10 +0100

    v0.9 - Posible version final. Repasar antes de entregar

--- app/src/main/java/com/example/imcapp/ImcResultActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcResultActivity.kt
@@ -1,12 +1,15 @@
 package com.example.imcapp
 
 import android.content.Intent
+import android.graphics.Color
 import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,6 +19,12 @@ class ImcResultActivity : AppCompatActivity() {
     private lateinit var tv_resultado_numerico:TextView
     private lateinit var tv_resultado_texto2:TextView
     private lateinit var btn_recalcularIMC:AppCompatButton
+
+
+    // Para debug en logcat:
+    private val TAG: String = "Test"
+    //Log.d(TAG, age.toString());
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,10 +72,42 @@ class ImcResultActivity : AppCompatActivity() {
 
     private fun setResultados()
     {
-        val calculo_IMC:Double? = intent.extras?.getDouble("EXTRA_IMC")
+        val calculo_IMC:Double = intent.extras?.getDouble("EXTRA_IMC")?:-1.0
 
-        tv_resultado_texto.text = "Hola wenas"
+
+        when
+        {
+            (calculo_IMC > 0.0 && calculo_IMC < 18.5) ->
+            {
+                tv_resultado_texto.text = getString(R.string.peso_insuficiente)
+                tv_resultado_texto.setTextColor(ContextCompat.getColor(this, R.color.inferior_red))
+                tv_resultado_texto2.text = getString(R.string.text_insuficiente)
+            }
+            (calculo_IMC >= 18.5 && calculo_IMC < 25.0) ->
+            {
+                tv_resultado_texto.text = getString(R.string.peso_normal)
+                tv_resultado_texto.setTextColor(ContextCompat.getColor(this, R.color.green))
+                tv_resultado_texto2.text = getString(R.string.text_normal)
+            }
+            (calculo_IMC >= 25.0 && calculo_IMC < 30) ->
+            {
+                tv_resultado_texto.text = getString(R.string.demasiado_peso)
+                tv_resultado_texto.setTextColor(ContextCompat.getColor(this, R.color.inferior_red))
+                tv_resultado_texto2.text = getString(R.string.text_pesoalto)
+            }
+            (calculo_IMC >= 30) ->
+            {
+                tv_resultado_texto.text = getString(R.string.sobrepeso)
+                tv_resultado_texto.setTextColor(ContextCompat.getColor(this, R.color.red))
+                tv_resultado_texto2.text = getString(R.string.text_obesidad)
+            }
+            else ->
+            {
+                tv_resultado_texto.text = "¿¿??"
+                tv_resultado_texto.setTextColor(ContextCompat.getColor(this, R.color.red))
+                tv_resultado_texto2.text = getString(R.string.text_error_IMC)
+            }
+        }
         tv_resultado_numerico.text = DecimalFormat("##.##").format(calculo_IMC)
-        tv_resultado_texto2.text = "Hola wenas que tal esto es un texto largo de prueba blablablablabla blablablablablalbalbalba lablba"
     }
 }
